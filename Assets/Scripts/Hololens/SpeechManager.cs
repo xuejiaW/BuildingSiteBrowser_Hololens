0b44390e7e1e6e6aceadbea729ce30eda668386a
wxj <wxjwangxuejia@outlook.com>
2017-10-08 17:02:07 +0800

    feat: add speech module
    Signed-off-by: wxj <wxjwangxuejia@outlook.com>

---
+++ Assets/Scripts/Hololens/SpeechManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : Singleton<SpeechManager>
+{
+    [System.Serializable]
+    public struct KeywordAndResponse
+    {
+        public string Keyword;
+        public UnityEvent Response;
+    }
+
+    public KeywordAndResponse[] KeywordsAndResponses;
+    private KeywordRecognizer keywordRecognizer;
+    private readonly Dictionary<string, UnityEvent> responses = new Dictionary<string, UnityEvent>();
+
+    private void Start()
+    {
+        int keywordCount = KeywordsAndResponses.Length;
+        if (keywordCount == 0)
+            return;
+        string[] keywords = new string[keywordCount];
+        for (int index = 0; index != keywordCount; ++index)
+        {
+            keywords[index] = KeywordsAndResponses[index].Keyword;
+            responses[keywords[index]] = KeywordsAndResponses[index].Response;
+        }
+        keywordRecognizer = new KeywordRecognizer(keywords);
+        keywordRecognizer.OnPhraseRecognized += keywordRecognizer_OnRecognized;
+
+        keywordRecognizer.Start();
+
+    }
+
+    private void keywordRecognizer_OnRecognized(PhraseRecognizedEventArgs args)
+    {
+        UnityEvent unityEvent;
+        if (responses.TryGetValue(args.text, out unityEvent))
+            unityEvent.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        keywordRecognizer.Stop();
+        keywordRecognizer.OnPhraseRecognized -= keywordRecognizer_OnRecognized;
+        keywordRecognizer.Dispose();
+    }
+}
